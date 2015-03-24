cef57223baac3323bd0f12d0c8261498ec77e804
Shota Fukumori (sora_h) <sorah@cookpad.com>
2015-03-24 15:44:16 +0900

    v1.1.0: support Etcd::Client#etcvault_keys

--- lib/etcd/etcvault.rb
+++ lib/etcd/etcvault.rb
@@ -12,7 +12,14 @@ module Etcd
         @etcvault_error = opts['_etcvault_error']
       end
     end
+
+    module ClientExtension
+      def etcvault_keys
+        api_execute('/_etcvault/keys', :get).body.each_line.map(&:chomp)
+      end
+    end
   end
 end
 
 Etcd::Node.__send__(:prepend, Etcd::Etcvault::NodeExtension)
+Etcd::Client.__send__(:prepend, Etcd::Etcvault::ClientExtension)
